50ace326e425beb18f4f0a94d7dea92b1114db0c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-09-29 11:49:11 +0300

    script for cloning all repositories

---
+++ scripts/git-clone-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REPOSITORIES=(FeshShop.Common FeshShop.Identity)
+
+if [ "$1" = "-p" ]
+  then
+    echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/FeshShop/{}.git'
+  else
+    for REPOSITORY in ${REPOSITORIES[*]}
+    do
+      cd ..
+      echo Cloning repository: $REPOSITORY
+      git clone https://github.com/FeshShop/$REPOSITORY.git
+    done
+fi
+
+read -p "Press any key to exit ..."
