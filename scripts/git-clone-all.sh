5ee61907fd58fd4eb62ef975baa2bc5296cd07b0
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2021-10-08 09:27:40 +0300

    change 'cd' command in git clone script

--- scripts/git-clone-all.sh
+++ scripts/git-clone-all.sh
@@ -5,9 +5,9 @@ if [ "$1" = "-p" ]
   then
     echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/FeshShop/{}.git'
   else
+    cd ..
     for REPOSITORY in ${REPOSITORIES[*]}
     do
-      cd ..
       echo Cloning repository: $REPOSITORY
       git clone https://github.com/FeshShop/$REPOSITORY.git
     done
